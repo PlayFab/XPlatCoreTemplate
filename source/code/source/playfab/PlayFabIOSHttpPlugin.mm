5d1b05f99eddef4365ac0313361cbf5db6b43fd3
Todd Bello <toddbell@microsoft.com>
2019-10-30 13:11:42 -0700

    Update PlayFabIOSHttpPlugin.mm (#32)
    matching change from android/linux to iOS.

--- source/code/source/playfab/PlayFabIOSHttpPlugin.mm
+++ source/code/source/playfab/PlayFabIOSHttpPlugin.mm
@@ -222,7 +222,7 @@ namespace PlayFab
         urlRequest.HTTPMethod = @"POST";
         SetPredefinedHeaders(requestTask, (__bridge void*)urlRequest);
 
-        auto headers = requestContainer.GetHeaders();
+        auto headers = requestContainer.GetRequestHeaders();
         if (headers.size() > 0)
         {
             for (auto const &obj : headers)
@@ -297,6 +297,8 @@ namespace PlayFab
         std::unique_ptr<Json::CharReader> jsonReader(jsonReaderFactory.newCharReader());
         JSONCPP_STRING jsonParseErrors;
         const bool parsedSuccessfully = jsonReader->parse(requestContainer.responseString.c_str(), requestContainer.responseString.c_str() + requestContainer.responseString.length(), &requestContainer.responseJson, &jsonParseErrors);
+
+        requestContainer.errorWrapper.RequestId = requestContainer.GetRequestId();
 
         if (parsedSuccessfully)
         {
