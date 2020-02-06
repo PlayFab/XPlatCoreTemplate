1540ff362277628bef9f051bbc1abf141156177b
Todd Bello <toddbell@microsoft.com>
2020-02-06 13:48:50 -0800

    Update set-gitmodules.sh
    fixing error checks

--- source/set-gitmodules.sh
+++ source/set-gitmodules.sh
@@ -6,17 +6,17 @@ if [ ".git" ] then
 else
 	git init
 	git submodule add https://github.com/open-source-parsers/jsoncpp.git ./external/jsoncpp
-	#if /I "%ERRORLEVEL%" gtr "1" (
-	#	exit /B %ERRORLEVEL%
-	#)
+	if [ $? != 0]  then
+		exit $?
+	fi
 fi
 
 pushd "external/jsoncpp"
 git checkout 2baad4923e6d9a7e09982cfa4b1c5fd0b67ebd87
 git reset --hard 2baad4923e6d9a7e09982cfa4b1c5fd0b67ebd87
-#if /I "%ERRORLEVEL%" neq "0" (
-#exit /B %ERRORLEVEL%
-#)
+if [ $? != 0] then
+	exit $?
+fi
 popd
 
 echo ========= Setting Git Submodules to specific commits (END) =========
