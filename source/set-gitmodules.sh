f748fe3895c67b3b74320cbc4d7b0149e434d00d
Todd Bello <toddbell@microsoft.com>
2020-02-06 13:56:00 -0800

    Update set-gitmodules.sh
    small updates that were required to be changed on a mac

--- source/set-gitmodules.sh
+++ source/set-gitmodules.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #echo ========= Setting Git Submodules to specific commits (BEGIN) =========
-if [ ".git" ] then
+if [ ".git" ]
+then
 	git submodule update --init --recursive
 else
 	git init
 	git submodule add https://github.com/open-source-parsers/jsoncpp.git ./external/jsoncpp
-	if [ $? != 0]  then
+	if [ $? != 0 ]
+	then
 		exit $?
 	fi
 fi
@@ -14,9 +16,10 @@ fi
 pushd "external/jsoncpp"
 git checkout 2baad4923e6d9a7e09982cfa4b1c5fd0b67ebd87
 git reset --hard 2baad4923e6d9a7e09982cfa4b1c5fd0b67ebd87
-if [ $? != 0] then
+if [ $? != 0 ]
+then
 	exit $?
 fi
 popd
 
-echo ========= Setting Git Submodules to specific commits (END) =========
+#echo ========= Setting Git Submodules to specific commits (END) =========
