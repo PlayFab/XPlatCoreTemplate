bc7836267fb939bb760d8efb775b5d1b91ae848b
pgilmorepf <paul@playfab.com>
2020-02-24 15:48:44 -0800

    Debugging Workspace

--- source/build/Linux/BuildTest.sh
+++ source/build/Linux/BuildTest.sh
@@ -4,6 +4,9 @@ set -e
 
 make all
 
+echo WORKSPACE = $WORKSPACE
+echo PF_TEST_TITLE_DATA_JSON = $PF_TEST_TITLE_DATA_JSON
+
 if [ -z "$PF_TEST_TITLE_DATA_JSON" ] || [ ! -f "$PF_TEST_TITLE_DATA_JSON" ]; then
     export PF_TEST_TITLE_DATA_JSON=${WORKSPACE}/JenkinsSdkSetupScripts/Creds/testTitleData.json
 fi
