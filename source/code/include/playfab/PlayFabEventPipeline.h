5e53402bdf995035a91993f0613e616af70f7356
Todd Bello <toddbell@microsoft.com>
2020-04-07 14:38:01 -0700

    Update PlayFabEventPipeline.h
    added comment to the actual PlayFabEventPipelineSettings class

--- source/code/include/playfab/PlayFabEventPipeline.h
+++ source/code/include/playfab/PlayFabEventPipeline.h
@@ -23,6 +23,9 @@ namespace PlayFab
 
     /// <summary>
     /// Settings for any event pipeline
+    /// NOTE: settings are expected to be set prior to calling PlayFabEventPipeline::Start()
+    /// changing them after PlayFabEventPipeline::Start() may cause threading issues
+    /// users should not expect changes made to settings to take effect after ::Start is called unless the pipeline is destroyed and re-created
     /// </summary>
     class PlayFabEventPipelineSettings
     {
