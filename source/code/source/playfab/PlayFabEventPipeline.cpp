05bf241b20dca3f494bb78b7e673f5e9664b9843
Todd Bello <toddbell@microsoft.com>
2020-04-07 11:37:38 -0700

    making general comment consistency

--- source/code/source/playfab/PlayFabEventPipeline.cpp
+++ source/code/source/playfab/PlayFabEventPipeline.cpp
@@ -69,9 +69,9 @@ namespace PlayFab
         }
     }
 
-    /// NOTE: settings are expected to be set prior to calling ::Start()
-    /// changing them after ::Start is called may cause threading issues
-    /// users should not expect changing settings to take effect unless a started pipeline is destroyed and re-created
+    // NOTE: settings are expected to be set prior to calling PlayFabEventPipeline::Start()
+    // changing them after PlayFabEventPipeline::Start() may cause threading issues
+    // users should not expect changes made to settings to take effect after ::Start is called unless the pipeline is destroyed and re-created
     std::shared_ptr<PlayFabEventPipelineSettings> PlayFabEventPipeline::GetSettings() const
     {
         return this->settings;
