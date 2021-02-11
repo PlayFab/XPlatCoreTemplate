87091fb466857102b1bf4339fb8b3b3da68396d9
Todd Bello <toddbell@microsoft.com>
2021-02-10 17:02:25 -0800

    Update make.js
    making function a simple variable that should get overloaded

--- make.js
+++ make.js
@@ -25,7 +25,7 @@ exports.makeCombinedAPI = function (apis, sourceDir, apiOutputDir) {
         vsYear: "2017", // As VS versions change, we may need to update this
         getVerticalNameDefault: getVerticalNameDefault,
         winSdkVersion: "10.0.17763.0", // Which version of the Windows SDK (A VS installation option) to use
-        ifNeedsExtraTests: ifNeedsExtraTests
+        ifNeedsExtraTests: "true"
     };
 
     templatizeTree(locals, path.resolve(sourceDir, "source"), apiOutputDir);
@@ -54,7 +54,7 @@ function makeApiFiles(api, sourceDir, apiOutputDir) {
         remStaticDefine: remStaticDefine,
         sdkVersion: sdkGlobals.sdkVersion,
         sortedClasses: getSortedClasses(api.datatypes),
-        ifNeedsExtraTests: ifNeedsExtraTests
+        ifNeedsExtraTests: "true"
     };
 
     var apihTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/PlayFab_Api.h.ejs"));
@@ -295,11 +295,6 @@ function ifHasProps(datatype, displayText) {
     if (datatype.properties.length === 0)
         return "";
     return displayText;
-}
-
-function ifNeedsExtraTests()
-{
-    return "true";
 }
 
 function getVerticalNameDefault() {
